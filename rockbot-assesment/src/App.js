f26848f71749f54fa279a7b5e668b0319aa00f1b
LarrySimiyu <larry.simiyu@gmail.com>
2021-04-05 13:27:34 -0700

    now playing component added alongside initial push

---
+++ rockbot-assesment/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+import NowPlaying from './components/NowPlaying'
+
+function App() {
+  return (
+    <div className="App">
+      <NowPlaying />
+     
+    </div>
+  );
+}
+
+export default App;
