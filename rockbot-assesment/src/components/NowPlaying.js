8f1ca77823c8b921156c364cfef113a8d7b16c4f
LarrySimiyu <larry.simiyu@gmail.com>
2021-04-06 12:12:53 -0700

    Now playing logic added, with general component boiler plate

--- rockbot-assesment/src/components/NowPlaying.js
+++ rockbot-assesment/src/components/NowPlaying.js
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, {useState, useEffect }from 'react'
+import axios from 'axios'
 
+
+const ENDPOINT = "https://s.rockbot.com/temp/now_playing.json"
 
 
 export default function NowPlaying(){
+    const [musicData, setMusicData] = useState([])
+    const [que, setQue] = useState([])
+    const [nowPlaying, setnowPlaying] = useState({})
+
+    const fetchData = () => {
+        return axios
+            .get(ENDPOINT).then(({ data }) => {
+                console.log(data)
+                return data
+            })
+        
+    }
+
+    // update current playing based off interval - 30000
+ // update current que after removing first element
+   
+   const updateData = () => {
+   
+    if(que.length !== 0) {
+        setnowPlaying(que[0])
+        console.log(nowPlaying)
+        que.shift()
+    }
+
+
+  
+}
+
+    useEffect(() => {
+        fetchData().then((response) => {
+            setMusicData(response)
+            setQue(response.aQueue)
+            setnowPlaying(response.aNowPlaying)
+
+
+            console.log(que.length)
+            if(que.length !== 0){
+                setInterval(() => {
+                    console.log("print")
+                    updateData()
+                }, 5000);
+          
+            }
+        })
+        
+    }, [])
+
+
+
     return (
-        <div>hello</div>
+        <div>
+            <h1>Now Playing</h1>
+            <p>{nowPlaying.sArtist} :             {nowPlaying.sSong}
+</p>
+            <p></p>
+            <h1>Coming Up</h1>
+            {que.map((queItem) => {
+                return <p>{queItem.sArtist} : {queItem.sSong} : {queItem.iLikes}</p>
+            })}
+
+            <button onClick={updateData}>update</button>
+
+            
+
+        </div>
     )
 }
