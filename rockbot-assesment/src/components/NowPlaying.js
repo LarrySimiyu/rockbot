c4254e3edfcea5f50606181fd1e95100e2b1a109
LarrySimiyu <larry.simiyu@gmail.com>
2021-04-16 10:21:25 -0700

    User can now upvote and downvote on songs

--- rockbot-assesment/src/components/NowPlaying.js
+++ rockbot-assesment/src/components/NowPlaying.js
@@ -1,61 +1,93 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const nowPlayingEndPoint = "https://api.rockbot.com/v3/engage/now_playing"
+// TODO: [] add liking and disliking functionality 
+// TODO: [] add duration timer
 
-export default function NowPlaying( {que, setQue}) {
 
 
+const nowPlayingEndPoint = "https://api.rockbot.com/v3/engage/now_playing";
+const voteUpEndPoint = "https://api.rockbot.com/v3/engage/vote_up"
+const voteDownEndPoint = "https://api.rockbot.com/v3/engage/vote_down"
+
+export default function NowPlaying({ que, setQue }) {
   const [nowPlaying, setnowPlaying] = useState({});
-  const [didUpdate, setdidUpdate] = useState(false);
 
   const fetchData = () => {
-    return axios.get(nowPlayingEndPoint, {
+    return axios
+      .get(nowPlayingEndPoint, {
+        headers: {
+          authorization: process.env.REACT_APP_API_KEY,
+        },
+        params: {
+          queue: 1,
+        },
+      })
+      .then(({ data }) => {
+        let response = data.response;
+        return response;
+      })
+      .then((response) => {
+        console.log(response.queue, "current queue");
+        console.log(response.now_playing.duration, "current duration")
+        setQue(response.queue);
+        setnowPlaying(response.now_playing);
+      });
+  };
+
+
+  const upVote = () => {
+     return axios.post(voteUpEndPoint, {}, {
       headers: {
         authorization: process.env.REACT_APP_API_KEY
       },
       params: {
-        queue: 1
+        pick_id: nowPlaying.pick_id
       }
-    }).then(({ data }) => {
-      return data.response;
-    });
+    }).then(( { data }) => {
+      console.log(data, "upvoted")
+      return data.response
+    })
+
   };
+  const downVote = () => {
+    return axios.post(voteDownEndPoint, {}, {
+      headers: {
+        authorization: process.env.REACT_APP_API_KEY
+      },
+      params: {
+        pick_id: nowPlaying.pick_id
+      }
+    }).then(( { data }) => {
+      console.log(data, "down voted")
+      return data.response
+    })
+
+  };
+
  
 
   // update data function checks for the length of the current que
   // if length is not zero we set the now playing state to be the first element in the que
   // from there we update our que array and change setdidupdate state to true
-  const updateData = () => {
-    if (que.length !== 0) {
-      setnowPlaying(que[0]);
-      console.log(nowPlaying);
-      que.shift();
-      setdidUpdate(true);
-    }
-  };
+  // const updateData = () => {
+  //   if (que.length !== 0) {
+  //     setnowPlaying(que[0]);
+  //     console.log(nowPlaying);
+  //     que.shift();
+  //     console.log(nowPlaying.remaining)
+  //   }
+  // };
 
-    // setInterval(() => {
-       // fetchData()
-    // }, 30000)
+  // setInterval(() => {
+  //    fetchData()
+  // }, 10000)
 
   // data is fetched when component mounts and set to state
   useEffect(() => {
-    fetchData().then((response) => {
-      console.log(response.queue, "current queue")
-      setQue(response.queue);
-      setnowPlaying(response.now_playing);
-
-      // interval timer calls updateData function every 30 seconds
-      // let interval = setInterval(() => {
-      //   updateData();
-      // }, 30000);
-      // clearInterval(interval);
-    });
-        // check for a change in the didUpdate value,  if state is changed rerender the component
-
-    setdidUpdate(false);
-  }, [didUpdate]);
+    fetchData();
+    // check for a change in the didUpdate value,  if state is changed rerender the component
+  }, []);
 
   return (
     <div className="nowPlayingPage">
@@ -71,6 +103,8 @@ export default function NowPlaying( {que, setQue}) {
         <div className="artistAndSongContainer">
           <div className="nowPlayingArtist">{nowPlaying.artist} </div>
           <div className="nowPlayingSong">{nowPlaying.song}</div>
+          <button onClick={() => upVote()}>Up</button>
+          <button onClick={() => downVote()}>Down</button>
         </div>
       </div>
 
