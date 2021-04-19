c27c78ab6254eafbf6645a7ad478847afdc98fe8
LarrySimiyu <larry.simiyu@gmail.com>
2021-04-18 18:16:53 -0700

    user can now directly vote on songs from queue

--- rockbot-assesment/src/components/NowPlaying.js
+++ rockbot-assesment/src/components/NowPlaying.js
@@ -1,14 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-// TODO: [] add liking and disliking functionality 
-// TODO: [] add duration timer
-
-
-
 const nowPlayingEndPoint = "https://api.rockbot.com/v3/engage/now_playing";
-const voteUpEndPoint = "https://api.rockbot.com/v3/engage/vote_up"
-const voteDownEndPoint = "https://api.rockbot.com/v3/engage/vote_down"
+const voteUpEndPoint = "https://api.rockbot.com/v3/engage/vote_up";
+const voteDownEndPoint = "https://api.rockbot.com/v3/engage/vote_down";
 
 export default function NowPlaying({ que, setQue }) {
   const [nowPlaying, setnowPlaying] = useState({});
@@ -29,43 +24,92 @@ export default function NowPlaying({ que, setQue }) {
       })
       .then((response) => {
         console.log(response.queue, "current queue");
-        console.log(response.now_playing.duration, "current duration")
+        console.log(response.now_playing.duration, "current duration");
         setQue(response.queue);
         setnowPlaying(response.now_playing);
       });
   };
 
-
-  const upVote = () => {
-     return axios.post(voteUpEndPoint, {}, {
-      headers: {
-        authorization: process.env.REACT_APP_API_KEY
-      },
-      params: {
-        pick_id: nowPlaying.pick_id
-      }
-    }).then(( { data }) => {
-      console.log(data, "upvoted")
-      return data.response
-    })
-
-  };
-  const downVote = () => {
-    return axios.post(voteDownEndPoint, {}, {
-      headers: {
-        authorization: process.env.REACT_APP_API_KEY
-      },
-      params: {
-        pick_id: nowPlaying.pick_id
-      }
-    }).then(( { data }) => {
-      console.log(data, "down voted")
-      return data.response
-    })
-
+  const upVoteNowPlaying = () => {
+    return axios
+      .post(
+        voteUpEndPoint,
+        {},
+        {
+          headers: {
+            authorization: process.env.REACT_APP_API_KEY,
+          },
+          params: {
+            pick_id: nowPlaying.pick_id,
+          },
+        }
+      )
+      .then(({ data }) => {
+        console.log(data, "upvoted now playing");
+        return data.response;
+      });
   };
 
- 
+  const downVoteNowPlaying = () => {
+    return axios
+      .post(
+        voteDownEndPoint,
+        {},
+        {
+          headers: {
+            authorization: process.env.REACT_APP_API_KEY,
+          },
+          params: {
+            pick_id: nowPlaying.pick_id,
+          },
+        }
+      )
+      .then(({ data }) => {
+        console.log(data, "down voted now playing");
+        return data.response;
+      });
+  };
+
+  const upVoteQueue = (pick_id) => {
+    return axios
+      .post(
+        voteUpEndPoint,
+        {},
+        {
+          headers: {
+            authorization: process.env.REACT_APP_API_KEY,
+          },
+          params: {
+            pick_id: pick_id,
+          },
+        }
+      )
+      .then(({ data }) => {
+        console.log(pick_id, "que pick_id");
+        console.log(data, "upvoted now playing");
+        return data.response;
+      });
+  };
+
+  const downVoteQueue = (pick_id) => {
+    return axios
+      .post(
+        voteDownEndPoint,
+        {},
+        {
+          headers: {
+            authorization: process.env.REACT_APP_API_KEY,
+          },
+          params: {
+            pick_id: pick_id,
+          },
+        }
+      )
+      .then(({ data }) => {
+        console.log(data, "down voted now playing");
+        return data.response;
+      });
+  };
 
   // update data function checks for the length of the current que
   // if length is not zero we set the now playing state to be the first element in the que
@@ -103,8 +147,8 @@ export default function NowPlaying({ que, setQue }) {
         <div className="artistAndSongContainer">
           <div className="nowPlayingArtist">{nowPlaying.artist} </div>
           <div className="nowPlayingSong">{nowPlaying.song}</div>
-          <button onClick={() => upVote()}>Up</button>
-          <button onClick={() => downVote()}>Down</button>
+          <button onClick={() => upVoteNowPlaying()}>Up</button>
+          <button onClick={() => downVoteNowPlaying()}>Down</button>
         </div>
       </div>
 
@@ -119,6 +163,10 @@ export default function NowPlaying({ que, setQue }) {
                   <div className="quedSong">{queItem.song}</div>
                 </div>
                 <div className="quedLikes"> + {queItem.likes}</div>
+                <button onClick={() => upVoteQueue(queItem.pick_id)}>Up</button>
+                <button onClick={() => downVoteQueue(queItem.pick_id)}>
+                  Down
+                </button>
               </div>
             );
           })}
