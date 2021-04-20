10965c094bd86125d06869ce1ae51bcfd11e8f91
LarrySimiyu <larry.simiyu@gmail.com>
2021-04-20 13:09:20 -0700

    now playing file code comments added

--- rockbot-assesment/src/components/NowPlaying.js
+++ rockbot-assesment/src/components/NowPlaying.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+
+// React fontawesome icons must be installed before running application
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faThumbsUp, faThumbsDown } from "@fortawesome/free-solid-svg-icons";
 
@@ -10,7 +12,11 @@ const voteDownEndPoint = "https://api.rockbot.com/v3/engage/vote_down";
 export default function NowPlaying({ que, setQue }) {
   const [nowPlaying, setnowPlaying] = useState({});
 
-  const fetchData = () => {
+  // To fetch now plasying data, set authorization header from .env
+  // Params requires a key of queue and a value of 1 for response object to return
+  // queued artists and songs
+
+  const fetchNowPlayingData = () => {
     return axios
       .get(nowPlayingEndPoint, {
         headers: {
@@ -25,10 +31,16 @@ export default function NowPlaying({ que, setQue }) {
         return response;
       })
       .then((response) => {
+        // After receiving data, state is set
         setQue(response.queue);
         setnowPlaying(response.now_playing);
       });
   };
+
+  // Voting requires an authorization header along side a pick_id passed into params
+  // as a value to target speciic song
+
+  // On page reload updated data (likes and dislikes) will be displayed
 
   const upVoteNowPlaying = () => {
     return axios
@@ -106,9 +118,9 @@ export default function NowPlaying({ que, setQue }) {
       });
   };
 
-  // data is fetched when component mounts and set to state
+  // When compnent mounts data is immediately fetched for rendering
   useEffect(() => {
-    fetchData();
+    fetchNowPlayingData();
   }, []);
 
   return (
@@ -170,8 +182,6 @@ export default function NowPlaying({ que, setQue }) {
           })}
         </div>
       </div>
-
-      {/* <button onClick={updateData}>update</button> */}
     </div>
   );
 }
